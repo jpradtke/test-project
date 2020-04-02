a6ebc18c3fde55b210a0fbe0d93931095dd132ac
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-02 15:01:36 +0200

    moved script to JS file

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("app.js is connected & live update is connected");
+console.log('About to fetch a rainbow!');
+    fetch('rainbow.jpeg')
+    .then(response=> {
+        console.log(response);
+        return response.blob();
+    })
+    .then(blob => {
+        console.log(blob);
+        document.getElementById('rainbow').src = URL.createObjectURL(blob);
+    })
+    .catch(error => {
+        console.log('error');
+        console.error(error);
+    });
